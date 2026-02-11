fbdcd24c4cd462fba1b0668bdeec9015655f83b5
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2026-02-11 15:41:01 +0100

    Update function to render shows

---
+++ render.js
@@ -0,0 +1,48 @@
+import { render } from "./script.js";
+export async function apifetchShow(){
+    const res = await fetch("https://api.tvmaze.com/shows");
+    const data = await res.json();
+    return data;
+}
+
+async function apiFetchEps(showID){
+    const render = document.querySelector(".rendering");
+    const res = await fetch(`https://api.tvmaze.com/shows/${showID}/episodes`);
+    if(res.ok){
+        const data = await res.json();
+        render.innerHTML ='';
+        return data;
+    }else{
+        render.innerText = `API data couldn't be rendered Error ${res.status}`;
+    }
+}
+
+export async function main(){
+    const allshows= await apifetchShow();
+    const selectShows = document.querySelector(".show-select");
+
+    allshows.map(element => {
+        const {name,id}=element;
+        const newShow = document.createElement("option");
+        newShow.id = id;
+        newShow.innerText = `${name}`;
+        selectShows.append(newShow);
+    });
+
+    let showID =1;
+    const allEpisodes=await apiFetchEps(showID);
+    render(allEpisodes);
+
+    selectShows.addEventListener("change", async (event) => {
+        const showtarget = event.target.value;
+
+        for(let i=0;i<allshows.length;i++){
+            if(allshows[i].name === showtarget){
+                showID = allshows[i].id;
+            }
+        }
+        const showEp = await apiFetchEps(showID);
+        render(showEp);
+    });
+
+}
--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-function render(){
-    const allEpisodes = getAllEpisodes();
-    allEpisodes.map(element => {
-        document.querySelector(".episodes-display").append(episodeComponent(element));
-    });
-    
+import {main} from './render.js';
 
+export function render(allEpisodes){
+        document.querySelector(".episodes-display").innerHTML = `
+                    <template class="episode-component">
+                    <h5 class="episode-nb-sn">S01E01</h5>
+                    <img class="episode-img" src="http://static.tvmaze.com/uploads/images/medium_landscape/1/2668.jpg" alt="episode-image"/>
+                    <h3 class="episode-title margin">Winter is Coming</h3>
+                    <p class="episode-summary margin">Lord Eddard Stark, ruler of the North, is summoned to court by his old friend, King Robert Baratheon, to serve as the King's Hand. Eddard reluctantly agrees after learning of a possible threat to the King's life. Eddard's bastard son Jon Snow must make a painful decision about his own future, while in the distant east Viserys Targaryen plots to reclaim his father's throne, usurped by Robert, by selling his sister in marriage.</p>
+                    <div class="episode-duration-rate">
+                        <p>62 min</p>
+                        <p>8.91</p>
+                    </div>
+                </template>
+    `;
+    function renderAll(){
+        allEpisodes.map(element => {
+        document.querySelector(".episodes-display").append(episodeComponent(element));
+    })};
+    
+    renderAll();
     const inputSearch = document.querySelector("#search-input");
     inputSearch.addEventListener("keyup", () =>{
-        filterEpisodes(inputSearch.value);
+        filterEpisodes(allEpisodes,inputSearch.value);
     }); 
 
-    const select = document.querySelector("select");
-    episodesList = allEpisodes.map(element => {
+
+    
+    const select = document.querySelector("#select-season");
+    const episodesList = allEpisodes.map(element => {
         const optionClone = document.querySelector(".season-select-option").cloneNode(true);
         optionClone.innerText = `${element.name}`;
         select.append(optionClone);
     });
 
+    select.addEventListener("change", (event) => {
+        const selectedOption = event.target.value;
+        selectedOption!='All Seasons' ? filterEpisodes(allEpisodes,selectedOption) : renderAll();
+    });
 }
 
 function episodeComponent(element){
@@ -33,7 +53,7 @@ function episodeComponent(element){
     return newArticle;
 }
 
-function filterEpisodes(input){
+function filterEpisodes(allEpisodes,input){
     document.querySelector(".episodes-display").innerHTML = `
                     <template class="episode-component">
                     <h5 class="episode-nb-sn">S01E01</h5>
@@ -46,7 +66,6 @@ function filterEpisodes(input){
                     </div>
                 </template>
     `;
-    const allEpisodes = getAllEpisodes();
     let count =0;
     allEpisodes.filter(element => {
         if((element.name.toUpperCase().includes(input.toUpperCase())) || (element.summary.toUpperCase().includes(input.toUpperCase()))){
@@ -57,5 +76,5 @@ function filterEpisodes(input){
         }
     });
 }
-
-render();
+ 
+main();
